2febee924433d0fd0f11e0645214f31773c6b6de
Slawek Mikula <slawek.mikula@gmail.com>
2016-12-08 15:38:31 +0100

    - building with gradle & Android Studio - update to OsmDroid 5.5 & Google Play Services 10.1

---
+++ FlightGearMap/app/src/main/java/com/juanvvc/flightgear/maps/SolidTileSource.java
@@ -0,0 +1,65 @@
+package com.juanvvc.flightgear.maps;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+
+import com.juanvvc.flightgear.MyLog;
+import com.juanvvc.flightgear.R;
+
+import org.osmdroid.tileprovider.MapTile;
+import org.osmdroid.tileprovider.tilesource.BitmapTileSourceBase.LowMemoryException;
+import org.osmdroid.tileprovider.tilesource.ITileSource;
+
+import java.io.InputStream;
+
+public class SolidTileSource implements ITileSource {
+	private Context context;
+
+	public SolidTileSource(Context c) {
+		this.context = c;
+	}
+
+	@Override
+	public Drawable getDrawable(String arg0) throws LowMemoryException {
+		MyLog.i(this, "Loading from file");
+		return context.getResources().getDrawable(R.drawable.solidtile);
+	}
+
+	@Override
+	public Drawable getDrawable(InputStream arg0) throws LowMemoryException {
+		MyLog.i(this, "Loading from stream");
+		return context.getResources().getDrawable(R.drawable.solidtile);
+	}
+
+	@Override
+	public int getMaximumZoomLevel() {
+		return 10;
+	}
+
+	@Override
+	public int getMinimumZoomLevel() {
+		return 2;
+	}
+
+	@Override
+	public String getTileRelativeFilenameString(MapTile arg0) {
+		MyLog.i(this, "Loading from tile");
+		return "";
+	}
+
+	@Override
+	public int getTileSizePixels() {
+		return 256;
+	}
+
+	@Override
+	public String name() {
+		return "SolidTileSource";
+	}
+
+	@Override
+	public int ordinal() {
+		return 1;
+	}
+
+}
